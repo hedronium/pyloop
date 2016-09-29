730885ae89361d4d4ede410e1c7c3f351d119d50
aniruddha <achakraborty510@gmail.com>
2016-09-29 16:47:00 +0600

    added screen coloring , terminal design, all commands added

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
 import click
+import os
+#from app import inits
+import sys
+from colorama import init
+from termcolor import cprint 
+from pyfiglet import figlet_format
+cprint(figlet_format('Pyloop!', font='starwars'),
+       'green', attrs=['bold'])
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+@click.group()
+def index():
 
-if __name__ == '__main__':
-    hello()
+	pass
+
+@index.command('install')
+def install(string):
+	""" Scripts that greets you """
+	print(string)
+
+
+@index.command('init')
+def init():
+	""" Initialize json file """ 
+	file_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+	
+	name 			= click.prompt('name', type=str) | file_name
+	version 		= click.prompt('version',type=str) | '1.0.0'
+	description 	= click.prompt('description',type=str)
+	author 			= click.prompt('author',type=str)
+	author_email 	= click.prompt('author email',type=str)
+	channels 		= click.prompt('Channels(comma separated)',type=str) | ['pip','pip3','pypy']
+
+@index.command('update')
+def update():
+	""" Updates from json folder """
+	return 0
+
+@index.command('clear')
+def clear():
+	""" Clear screens """
+	click.clear()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+			name="Pyloop",
+		version="0.01",
+		py_modules = ['app'],
+		install_requires=[
+			'Click',
+			'colorama',
+			'termcolor',
+			'pyfiglet'
+		],
+		entry_points = '''
+			[console_scripts]
+			pyloop=app:index
+		''',
+		author = 'Hedronium',
+		author_email= ''
+	)
