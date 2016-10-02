29af46f290a3ae0d567279174147d6646def4646
Aniruddha Chakraborty <achakraborty510@gmail.com>
2016-10-02 23:22:35 +0600

    slight change in version 0.12

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
 		name="pyloop",
-		version="0.10",
+		version="0.12",
 		py_modules = ['app'],
 		packages = ['pyloop'],
-		description='An universal pacakge manager for python 3',
+		description='An universal pacakge manager for python',
 		install_requires=[
 			'Click',
 			'colorama',
@@ -18,7 +18,7 @@ setup(
 			pyloop=pyloop.app:index
 		''',
 		url='https://github.com/Hedronium/pyloop',
-		download_url = 'https://github.com/Hedronium/pyloop/tarball/0.10',
+		download_url = 'https://github.com/Hedronium/pyloop/tarball/0.12',
 		author = 'Hedronium',
 		keywords = ['package manger','python 3'],
 		author_email= 'project.anik@gmail.com',
