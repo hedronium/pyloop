537d4cffd44012c4e4c00bee42d48c30a16cdce7
Aniruddha Chakraborty <achakraborty510@gmail.com>
2016-10-03 22:36:17 +0600

    new upadte

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 		name="pyloop",
-		version="0.17",
+		version="0.18",
 		py_modules = ['pyloop.app'],
 		packages = ['pyloop'],
 		description='An universal pacakge manager for python',
@@ -18,7 +18,7 @@ setup(
 			pyloop=pyloop.app:index
 		''',
 		url='https://github.com/Hedronium/pyloop',
-		download_url = 'https://github.com/Hedronium/pyloop/tarball/0.17',
+		download_url = 'https://github.com/Hedronium/pyloop/tarball/0.18',
 		author = 'Hedronium',
 		keywords = ['package manger','python 3'],
 		author_email= 'project.anik@gmail.com',
